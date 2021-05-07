e30f807596f8709754f8a8ee702736d0c27c23d6
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-05-07 10:06:42 +0530

    extends the new class from Queue class

--- Deque/src/ds/DeQue.java
+++ Deque/src/ds/DeQue.java
@@ -1,7 +1,86 @@
 package ds;
 
 
-public class DeQue {
+import java.util.Arrays;
+
+class Queue {
+
+    private int array[];
+
+    public void enQueue(int number){
+        if(array == null){
+            array = new int[1];
+            array[0] = number;
+        }else {
+            int[] temp = new int[array.length + 1];
+            for (int i = 0; i < array.length; i++) {
+                temp[i] = array[i];
+            }
+            temp[temp.length - 1] = number;
+            array = temp;
+        }
+    }
+
+    public void deQueue(){
+        if(array == null){
+            System.out.println("Queue is empty");
+            return;
+        }else if (array.length == 1){
+            array = null;
+            return;
+        }
+        int temp[] = new int[array.length -1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = array[i+1];
+        }
+        array = temp;
+    }
+
+    public boolean empty(){
+        if (array == null) return true;
+        else return false;
+    }
+
+    public int peek(){
+        if (array == null){
+            return 0;
+        }
+        return array[array.length -1];
+    }
+
+    public void clear(){
+        array = null;
+    }
+
+    public void print(){
+        if (array == null){
+            System.out.println("Queue is Empty");
+            return;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public boolean contains(int number){
+        if (array.length == 0){
+            System.out.println("Queue is empty");
+            return false;
+        }else{
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == number){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public int size(){
+        return empty() ? 0 : array.length;
+    }
+}
+
+
+public class DeQue extends Queue{
 
     public void enqueueRear(int number){
 
--- Deque/src/ds/TestDeque.java
+++ Deque/src/ds/TestDeque.java
@@ -1,4 +1,15 @@
 package ds;
 
 public class TestDeque {
+
+    public static void main(String[] args) {
+        DeQue deQue = new DeQue();
+        deQue.enQueue(10);
+        deQue.enQueue(20);
+        deQue.enQueue(30);
+        deQue.print();
+        deQue.enqueueRear(20);
+        deQue.enqueueRear(10);
+        deQue.print();
+    }
 }
